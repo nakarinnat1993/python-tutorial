f0ff8ec2940c2cf5b932d6ba24498ad8a8e98967
Nakarin Jaiseengam <nakarinnat1993@gmail.com>
2025-03-03 09:56:52 +0700

    Add List, Tuple, Set, Dictionary at 02Variables

--- 01variables.py
+++ 01variables.py
@@ -3,4 +3,33 @@ name = "John" # name is of type string
 print(x)
 print(name)
 
+# List 
+fruits = ["apple", "banana", "cherry"]
+numbers = [1, 2, 3, 4, 5]
+print("List: ", fruits, numbers)
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+result_add = list1 + list2  # result [1, 2, 3, 4, 5, 6]
+
+list3 = [1, 2, 3]
+result_mul = list3 * 3  # result [1, 2, 3, 1, 2, 3, 1, 2, 3]
+
+# Tuple likes List but it doesn't update
+fruits = ("apple", "banana", "cherry")
+numbers = (1, 2, 3, 4, 5)
+print("Tuple: ", fruits, numbers)
+
+# Set doesn't duplicate
+fruits = {"apple", "banana", "cherry"}
+numbers = {1, 2, 3, 4, 5}
+print("Set: ", fruits, numbers)
+
+# Dictionary (key: value)
+person = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+print("Dictionary: ", person)
+
 
