2f9f70b6c60cf7930fcff624e7be76c3c70134bd
Nakarin Jaiseengam <nakarinnat1993@gmail.com>
2025-03-17 16:50:42 +0700

    PyQt6: Horizontal Box Layout

---
+++ 09pyqt6/03hboxlayout.py
@@ -0,0 +1,26 @@
+from PyQt6.QtCore import QCoreApplication
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Horizontal Box Layout")
+        
+        hbox = QHBoxLayout(self)
+        self.setLayout(hbox)
+        
+        h_btn1 = QPushButton("H1")
+        h_btn2 = QPushButton("H2")
+        
+        hbox.addWidget(h_btn1)
+        hbox.addWidget(h_btn2)
+        
+        
+        
+app = QCoreApplication.instance()
+if app is None:
+    app = QApplication([])
+window = MainWindow()
+window.show()
+app.exec()
